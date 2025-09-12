80fe78dae94de6698a403bf52da1a5365ddc97a3
Danijel <glisovic.danijel96@gmail.com>
2025-09-12 19:15:33 +0200

    Added User entity migrations.

---
+++ src/Exchange-Example/Exchange-Example-Api/Data/Migrations/20250912171449_UserEntity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Exchange_Example_Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    KeycloakId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Username = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Balance = table.Column<decimal>(type: "numeric(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_stocks_UserId",
+                table: "user_stocks",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_user_stocks_users_UserId",
+                table: "user_stocks",
+                column: "UserId",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_user_stocks_users_UserId",
+                table: "user_stocks");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_user_stocks_UserId",
+                table: "user_stocks");
+        }
+    }
+}
